19e4b99a999c7feee2cbaf9cb1771e9451203864
dwk601 <10829539@uvu.edu>
2022-09-24 21:43:37 -0600

    testing

---
+++ app.js
@@ -0,0 +1,11 @@
+const mon = 'Mon';
+const tue = 'Tue';
+const wed = 'Wed';
+const thu = 'Thu';
+const fri = 'Fri';
+const sat = 'Sat';
+const sun = 'Sun';
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+console.log(daysOfWeek);
